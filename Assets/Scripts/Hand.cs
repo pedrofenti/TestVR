c0d5303ce844c5e6256cf42358975970fded8bed
Pedro <pedro.fernandez@enti.cat>
2022-04-06 12:27:40 +0200

    Hay q volver a configurar todo
    Pero ya hay animaciones y se pueden coger objetos, mal pero se pueden

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    Animator animator;
+    SkinnedMeshRenderer mesh;
+
+    float gripTarget;
+    float triggerTarget;
+    float gripCurrent;
+    float triggerCurrent;
+
+    public float speed;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mesh = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat("Grip", gripCurrent);
+        }
+
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat("Trigger", triggerCurrent);
+        }
+    }
+
+    public void ToggleVisibilty()
+    {
+        mesh.enabled = !mesh.enabled;
+    }
+}
