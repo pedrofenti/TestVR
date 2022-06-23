7c772ba7016f30a748fa68b87a76f98c1ce5d5fe
Pedro Fernandez <pedro.fernandez@enti.cat>
2022-06-23 09:58:16 +0200

    Spawners de Ingredientes

--- Assets/Scripts/OrderKitchenController.cs
+++ Assets/Scripts/OrderKitchenController.cs
@@ -7,7 +7,13 @@ public class OrderKitchenController : MonoBehaviour
     [SerializeField] ComputerScreenController computerScreenController;
     [SerializeField] RecipesManager recipeManager;
     private RecipeController recipeController;
-    private GameObject tmp;
+    private GameObject tmpObjectInRecipe;
+
+    [Header("VFX")]
+    [SerializeField] ParticleSystem NegativeFiftyPoints;
+    [SerializeField] ParticleSystem FiftyPoints;
+    [SerializeField] ParticleSystem HundredPoints;
+    [SerializeField] ParticleSystem HundredFiftyPoints;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,25 +21,49 @@ public class OrderKitchenController : MonoBehaviour
         {
             // pillo lo que tenga el plato
             recipeController = other.gameObject.GetComponent<RecipeController>();
-            tmp = recipeController.GetObjectRecipe();
+            tmpObjectInRecipe = recipeController.GetObjectRecipe();
 
             // destruir el gameobject y el plato
             Destroy(other.transform.parent.gameObject);
             recipeController.DestroyRecipe();
 
-            if (recipeManager.CheckIfRecipeIsOnDemand(tmp)) 
+            if (recipeManager.CheckIfRecipeIsOnDemand(tmpObjectInRecipe)) 
             // comprobar si existe el pedido o no
             {
                 // sumar puntos
-                PointsManager._POINTS_MANAGER.ScorePoints(computerScreenController.GetFillAmountFromAnObject(tmp));
+                PointsManager._POINTS_MANAGER.ScorePoints(computerScreenController.GetFillAmountFromAnObject(tmpObjectInRecipe));
+
+                // emite la cantidad de puntos 
+                EmitParticle(computerScreenController.GetFillAmountFromAnObject(tmpObjectInRecipe));
 
                 // borrarla del canvas y de la lista de recetas
-                computerScreenController.DestoyRecipeFromList(tmp);
+                computerScreenController.DestoyRecipeFromList(tmpObjectInRecipe);
             }
             else
             {
                 PointsManager._POINTS_MANAGER.SubstractPoints(PointsManager._POINTS_MANAGER.failedRecipePoints);
+                EmitParticle(-1);
             }
+        }
+    }
+
+    private void EmitParticle(float value)
+    {
+        if (value > 0.6f)
+        {
+            HundredFiftyPoints.Play();
+        }
+        else if (value < 0.6f && value > 0.3f)
+        {
+            HundredPoints.Play();
+        }
+        else if (value < 0.3f && value >= 0f)
+        {
+            FiftyPoints.Play();
+        }
+        else
+        {
+            NegativeFiftyPoints.Play();
         }
     }
 }
