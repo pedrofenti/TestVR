7f6be94f0e7352b858c5740f0e5a78d97a421b10
Pedro <pedro.fernandez@enti.cat>
2022-07-19 19:35:31 +0200

    Imagen del tutorial

--- Assets/Scripts/GameOverCanvasController.cs
+++ Assets/Scripts/GameOverCanvasController.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverCanvasController : MonoBehaviour
 {
-    private float timer = 3;
+    [Tooltip("Less or equal to 5")]
+    [SerializeField] float timer;
     private string timeRemaining;
 
     [SerializeField] Text text;
+
+    [SerializeField] string LevelName;
+    [SerializeField] bool changeToLevel;
 
     void Update()
     {
         timer -= Time.deltaTime;
 
-        if (timer > 2) timeRemaining = "3";
+        if (timer > 4) timeRemaining = "5";
+        else if (timer > 3) timeRemaining = "4";
+        else if (timer > 2) timeRemaining = "3";
         else if (timer > 1) timeRemaining = "2";
         else if (timer > 0) timeRemaining = "1";
         else timeRemaining = "0";
 
         text.text = timeRemaining;
+
+        if (timeRemaining == "0" && changeToLevel!) Destroy(gameObject);
+        else SceneManager.LoadScene(LevelName);
     }
 }
